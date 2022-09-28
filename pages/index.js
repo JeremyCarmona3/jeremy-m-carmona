7fa2ad4111d73ae692a836702326a96da3c199ec
JeremyCarmona3 <jcarmona1@my.bcit.ca>
2022-09-27 19:44:07 -0700

    code cleanup

--- pages/index.js
+++ pages/index.js
@@ -12,52 +12,48 @@ export default function Home() {
     <div className='main-cont'>
       <div className='content'>
         <Navbar />
-        <div className='row'>
+        <div className='row grid-two'>
           <div className='col'>
+            <h2>Jeremy Carmona</h2>
+            <p className='intro-text'>
+              I am a recent graduate of the Digital Design and Development
+              program at the British Columbia Institute of Technology. During my
+              2 years at BCIT, I learned many things ranging from designing to
+              developing. Although I like designing, developing web and mobile
+              cross-platform applications was something I was more passionate
+              about. I aspire to be a full-stack developer later on in my
+              career.
+            </p>
             <div className='row grid-two'>
               <div className='col'>
-                <h2>Jeremy Carmona</h2>
-                <p className='intro-text'>
-                  I am a recent graduate of the Digital Design and Development
-                  program at the British Columbia Institute of Technology.
-                  During my 2 years at BCIT, I learned many things ranging from
-                  designing to developing. Although I like designing, developing
-                  web and mobile cross-platform applications was something I was
-                  more passionate about. I aspire to be a full-stack developer
-                  later on in my career.
-                </p>
-                <div className='row grid-two'>
-                  <div className='col'>
-                    <h2>Experience</h2>
-                    <div>
-                      <p>Goopter Holdings Ltd.</p>
-                      <p>Front-end Developer Intern</p>
-                      <p>April 2022-May 2022</p>
-                    </div>
-                  </div>
-                  <div className='col'>
-                    <h2>Projects</h2>
-                    <div>
-                      <p>MojiMeals</p>
-                      <p>Full-stack Developer</p>
-                      <p>February 2022-March 2022</p>
-                    </div>
-                    <div>
-                      <p>OnMe</p>
-                      <p>Front-end Developer</p>
-                      <p>September 2021-December 2021</p>
-                    </div>
-                  </div>
+                <h2>Experience</h2>
+                <div>
+                  <p>Goopter Holdings Ltd.</p>
+                  <p>Front-end Developer Intern</p>
+                  <p>April 2022-May 2022</p>
                 </div>
               </div>
-              <div className='polaroid-cont'>
-                <Polaroid
-                  image={Avatar}
-                  alt='Avatar of Jeremy'
-                  caption='Front-end Developer'
-                />
+              <div className='col'>
+                <h2>Projects</h2>
+                <div>
+                  <p>MojiMeals</p>
+                  <p>Full-stack Developer</p>
+                  <p>February 2022-March 2022</p>
+                </div>
+                <div>
+                  <p>OnMe</p>
+                  <p>Front-end Developer</p>
+                  <p>September 2021-December 2021</p>
+                </div>
               </div>
             </div>
+          </div>
+          <div className='polaroid-cont'>
+            <Polaroid
+              image={Avatar}
+              alt='Avatar of Jeremy'
+              caption='Front-end Developer'
+            />
           </div>
         </div>
         <div className='row grid-four'>
